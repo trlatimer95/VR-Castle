b6d9a399d987d754e5e8175eea9999d3f3fe60c8
Tyler Latimer <trlatimer95@gmail.com>
2022-11-15 11:22:49 -0800

    Finished guillotine custom interaction - not tested

--- Assets/Scripts/Guillotine.cs
+++ Assets/Scripts/Guillotine.cs
@@ -1,40 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Guillotine : XRGrabInteractable
 {
     public GameObject guillotineBlade;
-    public Vector3 startPos;
-    public Vector3 endPos;
-    public float angleToTrigger;
+    public Vector3 bladeStartPos;
+    public Vector3 bladeEndPos;
+    public float angleToTrigger = -60.0f;
     public float fallSpeed;
     public float returnSpeed;
 
-    private bool isFalling = false;
-    private bool isReturning = false;
+    private bool dropBlade = false;
+    private bool raiseBlade = false;
     private bool trackHandleRotation = false;
     
 
     void Start()
     {
-        startPos = guillotineBlade.transform.position;
+        bladeStartPos = guillotineBlade.transform.position;
     }
 
     void Update()
     {
-        
+        if (dropBlade)
+        {
+            if (guillotineBlade.transform.position.y > bladeEndPos.y)
+            {
+                guillotineBlade.transform.position = Vector3.MoveTowards(guillotineBlade.transform.position, bladeEndPos, fallSpeed * Time.deltaTime);
+            }
+            else
+            {
+                dropBlade = false;
+                raiseBlade = true;
+            }
+        }
+        else if (raiseBlade)
+        {
+            if (guillotineBlade.transform.position.y < bladeStartPos.y)
+            {
+                guillotineBlade.transform.position = Vector3.MoveTowards(guillotineBlade.transform.position, bladeStartPos, returnSpeed * Time.deltaTime);
+            }
+            else
+            {
+                raiseBlade = false;
+                if (isSelected)
+                {
+                    trackHandleRotation = true;
+                    StartCoroutine(CheckHandleRotation());
+                }
+            }
+        }
     }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         trackHandleRotation = true;
+        StartCoroutine(CheckHandleRotation());
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        trackHandleRotation = false;
     }
 
     IEnumerator CheckHandleRotation()
     {
-        yield return new WaitForSeconds(1);
+        while (trackHandleRotation)
+        {
+            if (transform.eulerAngles.y <= angleToTrigger)
+            {
+                trackHandleRotation = false;
+                dropBlade = true;
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.5f);
+            }              
+        }        
     }
 }
