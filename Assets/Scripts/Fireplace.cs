7b528e6ebb381e57a8fdad2325dc423bfc472021
jewelz ludwig <trlatimer95@gmail.com>
2022-12-25 22:26:32 -0800

    Modify some object sizes, added interactable fireplace

---
+++ Assets/Scripts/Fireplace.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Fireplace : XRSocketInteractor
+{
+    [SerializeField]
+    private GameObject fireGameObject;
+    [SerializeField]
+    private ParticleSystem fireParticles;
+    [SerializeField]
+    private ParticleSystem emberParticles;
+    [SerializeField]
+    private AudioSource fireSound;
+    [SerializeField]
+    private MeshRenderer[] logRenderers;
+
+    private int currLogIndex = 0;
+    private Material logMaterial;
+    private bool[] addedLogs;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (fireSound == null)
+            gameObject.GetComponentInChildren<AudioSource>();
+
+        logMaterial = logRenderers[0].material;
+        addedLogs = new bool[logRenderers.Length];
+    }
+
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        base.OnHoverEntered(args);
+
+        if (currLogIndex >= logRenderers.Length)
+            return;
+
+        if (args.interactorObject.transform.gameObject.CompareTag("Log") && logRenderers.Any(f => f.enabled == false))
+        {
+            logRenderers[currLogIndex].enabled = true;
+            logRenderers[currLogIndex].material = interactableHoverMeshMaterial;
+        }
+    }
+
+    protected override void OnHoverExited(HoverExitEventArgs args)
+    {
+        base.OnHoverExited(args);
+
+        if (currLogIndex >= logRenderers.Length)
+            return;
+
+        if (args.interactorObject.transform.gameObject.CompareTag("Log") && !addedLogs[currLogIndex])
+        {
+            logRenderers[currLogIndex].enabled = false;
+        }
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        if (currLogIndex >= logRenderers.Length)
+            return;
+
+        if (args.interactorObject.transform.gameObject.CompareTag("Log") && !addedLogs[currLogIndex])
+        {
+            logRenderers[currLogIndex].enabled = true;
+            logRenderers[currLogIndex].material = logMaterial;
+            addedLogs[currLogIndex] = true;
+            currLogIndex++;
+        }
+    }
+
+    private void StartFire()
+    {
+        fireGameObject.SetActive(true);
+        fireParticles.Play();
+        emberParticles.Play();
+        fireSound.Play();
+    }
+}
