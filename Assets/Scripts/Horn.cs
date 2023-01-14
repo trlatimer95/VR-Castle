a46cced94673b14a897c12289eb276e0dfdd58a1
Tyler Latimer <trlatimer95@gmail.com>
2023-01-13 18:07:46 -0800

    Added custom trumpet/horn interactable and other interactables, fixed issues with weapon rack

---
+++ Assets/Scripts/Horn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horn : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip[] hornSounds;
+
+    private void Awake()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            audioSource.PlayOneShot(hornSounds[Random.Range(0, hornSounds.Length - 1)]);
+        }
+    }
+}
