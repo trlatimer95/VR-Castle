dbaa72c19e990fa0871b9d758a2ad606bccba39c
Tyler Latimer <trlatimer95@gmail.com>
2023-02-01 16:17:44 -0800

    Added more sounds, cleaned up some unused sound files

---
+++ Assets/Scripts/SocketPlaySound.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SocketPlaySound : MonoBehaviour
+{
+    [SerializeField]
+    private XRSocketInteractor socket;
+    [SerializeField]
+    private AudioSource soundSource;
+
+    void Start()
+    {
+        if (socket == null)
+            socket = GetComponent<XRSocketInteractor>();
+        if (soundSource == null)
+            soundSource = GetComponent<AudioSource>();
+
+        socket.selectEntered.AddListener(PlaySound);
+        socket.selectExited.AddListener(n => soundSource.Stop());
+    }
+
+    private void PlaySound(SelectEnterEventArgs args)
+    {
+        if (!soundSource.isPlaying)
+            soundSource.Play();
+    }
+
+    //private void StopSound(SelectExitEventArgs args)
+    //{
+    //    soundSource.Stop();
+    //}
+}
